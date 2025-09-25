6c87d335fe039ef991f654bcfe0a11b2e6966e40
Josef Šindelář <sindelarj@oadomazlice.com>
2025-09-25 11:29:35 +0200

    prvni zmena

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Shrnutí</title>
 </head>
 <body>
-    
+    <div class="main">
+        <div class="head"><h1>PROJEKTY </h1></div>
+        <div class="main-img"></div>
+    </div>
+    <div class="main-2">
+        
+        
+    </div>
 </body>
 </html>
